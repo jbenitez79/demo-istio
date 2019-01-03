f1b3e54bf2e65fc628c56e7027daeff1755ede88
jbenitez79 <41079825+jbenitez79@users.noreply.github.com>
2019-01-03 12:53:08 -0300

    Create test_version.py

---
+++ tests/test_version.py
@@ -0,0 +1,13 @@
+python
+import pytest
+import app
+
+@pytest.fixture
+def client():
+    client = app.app.test_client()
+    yield client
+
+
+def test_version(client):
+    req = client.get('/')
+    assert req.data == app.version
